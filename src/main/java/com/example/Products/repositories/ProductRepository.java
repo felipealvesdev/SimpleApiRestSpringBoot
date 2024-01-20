5a301e78d941ec89797b371374f613a37d892705
Felipe Alves <felipealexandre1408@gmail.com>
2024-01-20 12:51:50 -0300

    First commit with get, post, put and delete working

---
+++ src/main/java/com/example/Products/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.Products.repositories;
+
+import com.example.Products.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
